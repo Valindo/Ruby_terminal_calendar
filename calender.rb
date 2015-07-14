ba80639b84b9de25e8a862b05060721ccd8a939d
valindo <valindo.godinho@gmail.com>
2015-07-14 16:28:21 +0530

    Finished the Traversing and stuff

--- calender.rb
+++ calender.rb
@@ -1,5 +1,6 @@
 require 'date'
 
+
 #Format : Month-Year
 # 1-Jan....12-Dec
 def print_month_n_year(date_object)
@@ -13,24 +14,45 @@ def print_weekdays(start_day_of_week)
 	last_day_of_week = (start_day_of_week + 6)%7
 	days_of_week = Date::ABBR_DAYNAMES
 	while start_day_of_week%7 != last_day_of_week
-		print "#{days_of_week[start_day_of_week%7]}\t"
+		print "#{days_of_week[start_day_of_week%7]}"
+		print "\t"
 		start_day_of_week+=1
 	end
-	print "#{days_of_week[start_day_of_week%7]}\n"
+	print "#{days_of_week[start_day_of_week%7]}"
+	puts ""
 end
 
 def print_previous_dates(date_object, start_day_of_week)
-	month_start_day = date_object.wday - start_day_of_week
-	if month_start_day != start_day_of_week
-		date_object = date_object - month_start_day
-		for i in (0...month_start_day) do
-			print "#{date_object.day}*\t"
-			date_object = date_object +1
+	previous_month = date_object - 1
+	if date_object.wday != start_day_of_week
+		while previous_month.wday != start_day_of_week
+			previous_month = previous_month - 1
+		end
+
+		while previous_month.wday != date_object.wday
+			print "*#{previous_month.day}"
+			print "\t"
+			previous_month = previous_month + 1
 		end
 	end
 end
 
-
+def print_next_dates(date_object,start_day_of_week)
+	end_day_of_week = (start_day_of_week + 6)%7
+	if date_object.wday == end_day_of_week
+		return
+	end
+	date_object = date_object + 1
+	if date_object.wday != end_day_of_week
+		while date_object.wday != end_day_of_week
+			print "*#{date_object.day}"
+			print "\t"
+			date_object = date_object + 1
+		end
+		print "*#{date_object.day}"
+		print "\t"
+	end
+end
 
 #prints the calender which includes the headers ( month, year, days of the week)
 def print_calender(date_object, start_day_of_week)
@@ -46,20 +68,41 @@ def print_calender(date_object, start_day_of_week)
 	print_previous_dates(date_object,start_day_of_week)
 
 	while date_object.day != last_day.day
-		print "#{date_object.day}\t"
-		if end_day_of_week == date_object.wday
-			puts ""
-		end
+		print "#{date_object.day}"
+		print "\t" 
+		puts "" if end_day_of_week == date_object.wday
 		date_object = date_object + 1
 	end
-	
-	puts ""
+	print "#{date_object.day}"
+	print "\t"
+	print_next_dates(date_object,start_day_of_week)
 
+	puts ""
 end
 
 
 #test calls
+dow = 1
 date = Date.today
-for i in (0..6)
-	print_calender(date, i)
+print_calender(date, dow)
+
+
+while 1
+	puts "p - previouse\nn - next\nw - change dow\ne - exit"
+	choice = gets.chomp
+	case choice
+	when 'P','p'
+		date = date << 1
+		print_calendar(date, dow)
+	when 'N','n'
+		date = date << -1
+		print_calender(date, dow)
+	when 'W','w'
+		puts "0-sun.....6-sat"
+		dow = gets.chomp.to_i
+		print_calender(date, dow)
+	when 'E','e'
+		break
+	end
 end
+
