c1fc0cfc8a01cca1473ea8e1a5ef3847c88b6546
valindo <valindo.godinho@gmail.com>
2015-07-14 12:27:24 +0530

    Didn't fix print calender yet

--- calender.rb
+++ calender.rb
@@ -1,10 +1,14 @@
 require 'date'
 
-def print_month(dateobject)
+#Format : Month-Year
+# 1-Jan....12-Dec
+def print_month_n_year(date_object)
 	month = Date::ABBR_MONTHNAMES
-	puts "#{month[dateobject.month]}-#{dateobject.year}"
+	puts "\t\t\t#{month[date_object.month]}-#{date_object.year}"
 end
 
+#Format : Sun 	Mon 	Tue ....
+# 0-Sun....6-Sat
 def print_weekdays(start_day_of_week)
 	last_day_of_week = (start_day_of_week + 6)%7
 	days_of_week = Date::ABBR_DAYNAMES
@@ -15,13 +19,47 @@ def print_weekdays(start_day_of_week)
 	print "#{days_of_week[start_day_of_week%7]}\n"
 end
 
+def print_previous_dates(date_object, start_day_of_week)
+	month_start_day = date_object.wday - start_day_of_week
+	if month_start_day != start_day_of_week
+		date_object = date_object - month_start_day
+		for i in (0...month_start_day) do
+			print "#{date_object.day}*\t"
+			date_object = date_object +1
+		end
+	end
+end
 
+
+
+#prints the calender which includes the headers ( month, year, days of the week)
+def print_calender(date_object, start_day_of_week)
+	
+	#print the first few parts of the calender
+	print_month_n_year(date_object)
+	print_weekdays(start_day_of_week)
+
+	date_object = Date.new(date_object.year,date_object.month,1)
+	last_day = Date.new(date_object.year,date_object.month,-1)
+	end_day_of_week = (start_day_of_week + 6)%7
+
+	print_previous_dates(date_object,start_day_of_week)
+
+	while date_object.day != last_day.day
+		print "#{date_object.day}\t"
+		if end_day_of_week == date_object.wday
+			puts ""
+		end
+		date_object = date_object + 1
+	end
+	
+	puts ""
+
+end
 
 
 #test calls
 date = Date.today
-date = date << 1
-print_month(date)
 for i in (0..6)
-	print_weekdays(i)
+	print_calender(date, i)
 end
